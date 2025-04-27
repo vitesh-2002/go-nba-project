9a8bb2d5214fe7cf396c3c13e04e83fbc086de23
vitesh-2002 <vitesh2002@gmail.com>
2025-04-27 11:01:05 -0400

    feat: setting up db persistence

---
+++ database/database.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "host=localhost user=vitesh password=vitesh1107 dbname=postgres port=5432 sslmode=disable"
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	fmt.Println("📦 Database connection successful")
+	DB = database
+}
